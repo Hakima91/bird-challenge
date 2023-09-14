e3ba853337036f7c31285f2246360038101dafce
hakima amroun <hakimaterki91@gmail.com>
2023-09-13 21:11:45 -0400

    initial commit

---
+++ bird-challenge-code/Program.cs
@@ -0,0 +1,96 @@
+﻿Console.WriteLine("Bird hunter trying to catch drofsnar");
+// string = Console.ReadLine();
+// if("Bird hunter chops drfsnar");
+// Console.WriteLine(${Drofsnar})life is lost
+
+// else if("Drofsnar gets the stopper");
+// Console.WriteLine("the bird hunter become vulnerable");
+
+
+int Bird=10;
+Console.WriteLine(Bird);
+int CrestedIbis=100;
+Console.WriteLine(CrestedIbis);
+int GreatKiskudee=300;
+Console.WriteLine(GreatKiskudee);
+int RedneckedPhalarope=700;
+Console.WriteLine(RedneckedPhalarope);
+int EveningGrosbeak=1000;
+Console.WriteLine(EveningGrosbeak);
+int GreaterPrairieChicken=2000;
+Console.WriteLine(GreaterPrairieChicken);
+int IcelandGull=3000;
+Console.WriteLine(IcelandGull);
+int OrangeBelliedParrot=5000;
+Console.WriteLine(OrangeBelliedParrot);
+
+// string compositePoints=string.format("Birds points are 10+100+300+700+1000+2000+3000+5000");
+// Console.WriteLine(compositePoints);
+
+// int VulnerableBirdHunter=(200+400+800+1600 points);
+// Console.WriteLine=(VulnerableBirdHunter);
+string testdata = "Bird";
+if (testdata == "Bird")
+{
+   Console.WriteLine(Bird); 
+}
+else if (testdata == "CrestedIbis")
+{
+    Console.WriteLine(CrestedIbis);
+}
+else if (testdata == "GreatKiskudee")
+{
+   Console.WriteLine(GreatKiskudee); 
+}
+
+else if (testdata == "RedCrossbill")
+{
+  Console.WriteLine(RedCrossbill);  
+}
+else if (testdata == "EveningGrosbeak")
+{
+   Console.WriteLine(EveningGrosbeak);   
+}
+else if (testdata == "GreaterPrairieChicken")
+{
+     Console.WriteLine(GreaterPrairieChicken);
+}
+
+else if (testdata == "IcelandGull")
+{
+   Console.WriteLine(IcelandGull); 
+}
+
+else if (testdata == "OrangebelliedParrope")
+{
+   Console.WriteLine(OrangebelliedParrope); 
+}
+// else ("the second bird worth 400 points");
+// Console.WriteLine("the second bird");
+
+// else if ("the third bird worth 30 points")
+// Console.WriteLine("third bird");
+
+// // string[] total value = new value[] {"200+400+30"};
+// // foreach(string total value)
+
+// Console.WriteLine("Drofsnal get additional life");
+
+// using class
+
+// birdspoints = newbirds();
+//  CrestedLbis = "100";
+// Console.WriteLine(CrestedLbis);
+// RedneckedPhalarope= "700";
+// Console.WriteLine(RednekedPhalarope);
+
+// Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,InvincibleBirdHunte
+// r,EveningGrosbeak,GreaterPrairieChicken,VulnerableBirdHunter,VulnerableBir
+// dHunter,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,IcelandGull,Cres
+// tedIbis,GreatKiskudee,InvincibleBirdHunter,Bird,Bird,Bird,Bird,Bird,Bird,B
+// ird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,
+// Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird,Bird
+// ,Bird,Bird,Bird,Bird,RedCrossbill,Red-
+// neckedPhalarope,InvincibleBirdHunter,VulnerableBirdHunter,Orange-
+// belliedParrot,InvincibleBirdHunter,Bird,Bird,Bird,Bird,Bird,VulnerableBird
+// Hunter
